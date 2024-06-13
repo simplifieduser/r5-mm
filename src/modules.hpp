6b410f5b1edcc00f2c7ce8de988a7debcbe2297a
ElenaRF <145588974+ElenaRF@users.noreply.github.com>
2024-06-13 07:05:05 +0000

    1

--- src/modules.hpp
+++ src/modules.hpp
@@ -4,5 +4,12 @@
 #include <systemc>
 using namespace sc_core;
 
+SC_MODULE(GET_ADDRESS)
+{
+    sc_in<uint32_t> virtual_address;
+    sc_in<unsigned> tlbSize;
+    sc_in<unsigned> tlbsLatency;
+    sc_in<unsigned> blocksize;
+};
 
 #endif
