e4336f68a5f704c4e64e9260110190e3783cbaaa
ElenaRF <145588974+ElenaRF@users.noreply.github.com>
2024-06-21 09:18:58 +0000

    last module and run_simulation method

--- src/modules.cpp
+++ src/modules.cpp
@@ -10,16 +10,43 @@ struct Result
     size_t primitive_gate_count;
 };
 
-struct Request
-{
-    uint32_t addr;
-    uint32_t data;
-    int we;
-};
-
 Result run_simulation(int cycles, unsigned tlbSize, unsigned tlbsLatency, unsigned blocksize, unsigned v2bBlockOffset, unsigned memoryLatency, size_t numRequests, Request *requests, const char *tracefile)
 {
-    Result result;
+    Result result;            // to be returned
+    uint32_t buffer[tlbSize]; // stores which virtual addresses are in the tlb
+    sc_clock clk;
+
+    sc_signal<size_t> cycles_count, misses, hits, primitive_gate_count;
+    sc_signal<bool> finished; // marks, when all the requests have been processed
+
+    REQUEST_PROCESSOR request_processor("request_processor", tlbSize, tlbsLatency, blocksize, v2bBlockOffset, memoryLatency, numRequests, requests, buffer);
+
+    request_processor.clk.bind(clk);
+    request_processor.cycles.bind(cycles_count);
+    request_processor.misses.bind(misses);
+    request_processor.hits.bind(hits);
+    request_processor.primitive_gate_count.bind(primitive_gate_count);
+    request_processor.finished.bind(finished);
+
+    sc_start();
+    while (finished.read() == false)
+    {
+        // wait, until all requests have been processed
+    }
+
+    // set result values to output from request_processor
+    result.hits = hits.read();
+    result.misses = misses.read();
+    result.primitive_gate_count = primitive_gate_count.read();
+
+    if (cycles_count.read() > cycles)
+    {
+        result.cycles = INT_MAX;
+    }
+    else
+    {
+        result.cycles = cycles_count.read();
+    }
 
     return result;
 }
--- src/modules.hpp
+++ src/modules.hpp
@@ -12,19 +12,17 @@ struct Request
     int we;
 };
 
-SC_MODULE(TLB)
+SC_MODULE(ADDRESS_GETTER)
 {
     // input
     sc_in<bool> clk;
     sc_in<uint32_t> virtual_address;
+    sc_in<bool> start;
 
     // value at index i in the buffer stores a tag that is currently cached in the tlb
     uint32_t *buffer;
-    unsigned tlbs_latency;
-    unsigned memory_latency;
-    unsigned blocksize;
-    unsigned v2b_block_offset;
-    unsigned tlb_size;
+    // these values are set once and don't change
+    unsigned tlbs_latency, memory_latency, blocksize, v2b_block_offset, tlb_size;
 
     // true as soon as a valid physical address has been written to physical_address out-signal
     sc_out<bool> finished;
@@ -36,9 +34,9 @@ SC_MODULE(TLB)
 
     int latency;
 
-    SC_CTOR(TLB);
+    SC_CTOR(ADDRESS_GETTER);
 
-    TLB(sc_module_name name, uint32_t * buffer, unsigned tlb_size, unsigned tlbs_latency, unsigned blocksize, unsigned v2b_block_offset, unsigned memory_latency) : sc_module(name), buffer(buffer), tlb_size(tlb_size), tlbs_latency(tlbs_latency), blocksize(blocksize), v2b_block_offset(v2b_block_offset), memory_latency(memory_latency)
+    ADDRESS_GETTER(sc_module_name name, uint32_t * buffer, unsigned tlb_size, unsigned tlbs_latency, unsigned blocksize, unsigned v2b_block_offset, unsigned memory_latency) : sc_module(name), buffer(buffer), tlb_size(tlb_size), tlbs_latency(tlbs_latency), blocksize(blocksize), v2b_block_offset(v2b_block_offset), memory_latency(memory_latency)
     {
         SC_THREAD(count_latency);
         sensitive << clk.pos();
@@ -48,6 +46,11 @@ SC_MODULE(TLB)
     {
         while (true)
         {
+            // wait for start_signal
+            while (start->read() == false)
+            {
+                wait();
+            }
             // not actually necessary, but will let the clock run for latency cycles and then set the values for the new request
 
             set_values();
@@ -98,17 +101,18 @@ SC_MODULE(MAIN_MEMORY)
     sc_in<bool> we;
     sc_in<uint32_t> data;
     sc_in<uint32_t> address;
+    sc_in<bool> got_address; // true, as soon as correct address has been calculated, can then start writing/reading data
 
     unsigned memory_latency;
     // stores address as key and data as value
-    std::map<uint32_t, uint32_t> mem_map;
+    std::map<uint32_t, char> mem_map;
 
     sc_out<bool> finished;
     sc_out<uint32_t> out_data;
 
     SC_CTOR(MAIN_MEMORY);
 
-    MAIN_MEMORY(sc_module_name name, std::map<uint32_t, uint32_t> & map, unsigned memory_latency) : sc_module(name), mem_map(map), memory_latency(memory_latency)
+    MAIN_MEMORY(sc_module_name name, std::map<uint32_t, char> & map, unsigned memory_latency) : sc_module(name), mem_map(map), memory_latency(memory_latency)
     {
         SC_THREAD(start_process);
         sensitive << clk.pos();
@@ -118,7 +122,12 @@ SC_MODULE(MAIN_MEMORY)
     {
         while (true)
         {
+            while (got_address->read() == false)
+            {
+                wait();
+            }
             update_memory();
+
             int cycle_counter = 0;
             finished->write(false);
 
@@ -165,16 +174,75 @@ SC_MODULE(MAIN_MEMORY)
 SC_MODULE(REQUEST_PROCESSOR)
 {
     sc_in<bool> clk;
-    TLB address_getter;
+
+    // fixed values for this tlb
+    unsigned tlb_size, tlbs_latency, blocksize, v2b_block_offset, memory_latency;
+    size_t num_requests;
+    Request *requests;
+
+    ADDRESS_GETTER address_getter;
     MAIN_MEMORY data_manager;
-    uint32_t *buffer;
-    std::map<uint32_t, uint32_t> mem_map;
+    uint32_t *buffer;                 // stores which virtual addresses are currently in tlb
+    std::map<uint32_t, char> mem_map; // represents the main memory
+
+    sc_signal<uint32_t> current_virt_address, current_phys_address, data;
+    sc_signal<bool> get_address_finished, manage_data_finished, we, start_request;
 
     sc_out<size_t> cycles, misses, hits, primitive_gate_count;
+    sc_out<bool> finished;
 
     SC_CTOR(REQUEST_PROCESSOR);
-    REQUEST_PROCESSOR(sc_module_name name, unsigned tlb_size, unsigned tlbs_latency, unsigned blocksize, unsigned v2b_block_offset, unsigned memory_latency, size_t num_requests, Request *requests) : sc_module(name), buffer(new uint32_t[tlb_size]), address_getter("address_getter", buffer, tlb_size, tlbs_latency, blocksize, v2b_block_offset, memory_latency), data_manager("data_manager", mem_map, memory_latency)
+    REQUEST_PROCESSOR(sc_module_name name, unsigned tlb_size, unsigned tlbs_latency, unsigned blocksize, unsigned v2b_block_offset, unsigned memory_latency, size_t num_requests, Request *requests, uint32_t *buffer_param) : sc_module(name), tlb_size(tlb_size), tlbs_latency(tlbs_latency), blocksize(blocksize), v2b_block_offset(v2b_block_offset), memory_latency(memory_latency), num_requests(num_requests), requests(requests), buffer(buffer_param), address_getter("address_getter", buffer_param, tlb_size, tlbs_latency, blocksize, v2b_block_offset, memory_latency), data_manager("data_manager", mem_map, memory_latency)
     {
+        address_getter.clk.bind(clk);
+        address_getter.virtual_address.bind(current_virt_address);
+        address_getter.physical_address.bind(current_phys_address);
+        address_getter.finished.bind(get_address_finished);
+        address_getter.start.bind(start_request);
+
+        data_manager.clk.bind(clk);
+        data_manager.address.bind(address_getter.physical_address);
+        data_manager.data.bind(data);
+        data_manager.we.bind(we);
+        data_manager.got_address.bind(address_getter.finished);
+
+        finished->write(false);
+
+        SC_THREAD(run_program);
+    }
+
+    void run_program()
+    {
+        for (int i = 0; i < num_requests; i++)
+        {
+            // set signals according to values in current request
+            data.write(requests[i].data);
+            we.write(requests[i].we);
+            current_virt_address.write(requests[i].addr);
+            start_request.write(true);
+
+            // first, wait for correct physical address to be fetched
+            while (get_address_finished == false)
+            {
+                wait();
+            }
+
+            // now update with returned values
+            cycles->write(cycles->read() + address_getter.cycles->read());
+            hits->write(hits->read() + address_getter.hit->read());
+            misses->write(misses->read() + !address_getter.hit->read());
+            start_request.write(false);
+
+            // wait, while data is being read/written
+            while (manage_data_finished == false)
+            {
+                wait();
+            }
+            // update cycles again
+            cycles->write(cycles->read() + memory_latency);
+        }
+
+        finished->write(true);
     }
 };
 #endif
