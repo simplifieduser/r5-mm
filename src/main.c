ab226bfc550aad8afafa797ae52ffa2b46d5d433
Jonah Zabel <develop.polony@gmx.net>
2024-07-06 16:53:05 +0200

    Formated code

--- src/main.c
+++ src/main.c
@@ -7,127 +7,127 @@
 #include "messages.h"
 #include "file_parser.h"
 
-int main(int argc, char*argv[]) {
+int main(int argc, char *argv[]) {
     int cycles = 10000;
     unsigned int tlbSize = 64;
     unsigned int tlbLatency = 1;
     unsigned int blocksize = 4;
     unsigned int v2bBlockOffset = 4;
     unsigned int memoryLatency = 100;
-    const char* tracefile = NULL;
-    const char* inputfile = NULL;
+    const char *tracefile = NULL;
+    const char *inputfile = NULL;
     /*
      * die Werte von v2bBlockOffset und cycles wurden beliebig gewählt, da es in der Realität keinen v2bBlockOffset gibt
      * alle anderen Werte stammen von: Patterson, D. A., Hennessy, J. L. (). Computer Organization and Design: The Hardware/Software Interface. (4th ed.). Morgan Kaufman. Seite 503.
     */
     int opt = 0;
     static struct option long_options[] = {
-            {"cycles", required_argument, 0, 'c'},
-            {"blocksize", required_argument, 0, 'b'},
+            {"cycles",           required_argument, 0, 'c'},
+            {"blocksize",        required_argument, 0, 'b'},
             {"v2b-block-offset", required_argument, 0, 'o'},
-            {"tlb-size",required_argument,0,'s'},
-            {"tlb-latency", required_argument,0,'t'},
-            {"memory-latency", required_argument,0,'m'},
-            {"tf",required_argument,0,'f'},
-            {"help", no_argument,0,'h'},
-            {0,0,0,0}
+            {"tlb-size",         required_argument, 0, 's'},
+            {"tlb-latency",      required_argument, 0, 't'},
+            {"memory-latency",   required_argument, 0, 'm'},
+            {"tf",               required_argument, 0, 'f'},
+            {"help",             no_argument,       0, 'h'},
+            {0, 0,                                  0, 0}
     };
     opterr = 0; // Stellt die Fehlermeldungen von getopt_long still, um eigene Fehlermeldungen auszugeben
 
     // Durchsucht die übergebenen Optionen ob Hilfe gedruckt werden soll
     for (int i = 1; i < argc; i++) {
-        if (strncmp(argv[i], "-h",2) == 0 || strncmp(argv[i], "--help",6) == 0) {
-            (void) fprintf(stderr,HELP_MSG"\n");
+        if (strncmp(argv[i], "-h", 2) == 0 || strncmp(argv[i], "--help", 6) == 0) {
+            (void) fprintf(stderr, HELP_MSG"\n");
             return EXIT_SUCCESS;
         }
     }
 
     // Einlesen der Optionen/Argumente
-    while ((opt = getopt_long(argc, argv, ":c:b:o:s:t:m:f:h", long_options, NULL))!= -1){
-        switch (opt){
-            case 'c':{
+    while ((opt = getopt_long(argc, argv, ":c:b:o:s:t:m:f:h", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'c': {
                 char *endOfPointer = NULL;
                 errno = 0;
-                long tmp = strtol(optarg,&endOfPointer,10);
-                if(errno != 0|| *endOfPointer != '\0'|| tmp > INT32_MAX || tmp<0){
-                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_CYCLES"\n");
+                long tmp = strtol(optarg, &endOfPointer, 10);
+                if (errno != 0 || *endOfPointer != '\0' || tmp > INT32_MAX || tmp < 0) {
+                    (void) fprintf(stderr, ILLEGAL_ARGUMENT_CYCLES"\n");
                     return EXIT_FAILURE;
                 }
                 cycles = (int) tmp;
                 break;
             }
-            case 'b':{
+            case 'b': {
                 char *endOfPointer = NULL;
                 errno = 0;
-                long tmp = strtol(optarg,&endOfPointer,10);
-                if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<=0){
-                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_BLOCKSIZE"\n");
+                long tmp = strtol(optarg, &endOfPointer, 10);
+                if (errno != 0 || *endOfPointer != '\0' || tmp > UINT32_MAX || tmp <= 0) {
+                    (void) fprintf(stderr, ILLEGAL_ARGUMENT_BLOCKSIZE"\n");
                     return EXIT_FAILURE;
                 }
                 blocksize = (unsigned) tmp;
                 break;
             }
-            case 'o':{
+            case 'o': {
                 char *endOfPointer = NULL;
                 errno = 0;
-                long tmp = strtol(optarg,&endOfPointer,10);
-                if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_V2B_BLOCK_OFFSET"\n");
+                long tmp = strtol(optarg, &endOfPointer, 10);
+                if (errno != 0 || *endOfPointer != '\0' || tmp > UINT32_MAX || tmp < 0) {
+                    (void) fprintf(stderr, ILLEGAL_ARGUMENT_V2B_BLOCK_OFFSET"\n");
                     return EXIT_FAILURE;
                 }
                 v2bBlockOffset = (unsigned) tmp;
                 break;
             }
-            case 's':{
+            case 's': {
                 char *endOfPointer = NULL;
                 errno = 0;
-                long tmp = strtol(optarg,&endOfPointer,10);
-                if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_TLB_SIZE"\n");
+                long tmp = strtol(optarg, &endOfPointer, 10);
+                if (errno != 0 || *endOfPointer != '\0' || tmp > UINT32_MAX || tmp < 0) {
+                    (void) fprintf(stderr, ILLEGAL_ARGUMENT_TLB_SIZE"\n");
                     return EXIT_FAILURE;
                 }
                 tlbSize = (unsigned) tmp;
                 break;
             }
-            case 't':{
+            case 't': {
                 char *endOfPointer = NULL;
                 errno = 0;
-                long tmp = strtol(optarg,&endOfPointer,10);
-                if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_TLB_LATENCY"\n");
+                long tmp = strtol(optarg, &endOfPointer, 10);
+                if (errno != 0 || *endOfPointer != '\0' || tmp > UINT32_MAX || tmp < 0) {
+                    (void) fprintf(stderr, ILLEGAL_ARGUMENT_TLB_LATENCY"\n");
                     return EXIT_FAILURE;
                 }
                 tlbLatency = (unsigned) tmp;
                 break;
             }
-            case 'm':{
+            case 'm': {
                 char *endOfPointer = NULL;
                 errno = 0;
-                long tmp = strtol(optarg,&endOfPointer,10);
-                if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_MEMORY_LATENCY"\n");
+                long tmp = strtol(optarg, &endOfPointer, 10);
+                if (errno != 0 || *endOfPointer != '\0' || tmp > UINT32_MAX || tmp < 0) {
+                    (void) fprintf(stderr, ILLEGAL_ARGUMENT_MEMORY_LATENCY"\n");
                     return EXIT_FAILURE;
                 }
                 memoryLatency = (unsigned) tmp;
                 break;
             }
-            case 'f':{
+            case 'f': {
                 tracefile = optarg;
                 FILE *file = NULL; // Erstelle eine Datei mit den Namen der künftigen Tracefile
-                file = fopen(tracefile,"w");
-                if(file == NULL) {
-                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_TRACEFILE"\n");
+                file = fopen(tracefile, "w");
+                if (file == NULL) {
+                    (void) fprintf(stderr, ILLEGAL_ARGUMENT_TRACEFILE"\n");
                     return EXIT_FAILURE;
                 }
                 fclose(file);
                 break;
             }
-            case 'h':{
-                (void) fprintf(stderr,HELP_MSG"\n");
+            case 'h': {
+                (void) fprintf(stderr, HELP_MSG"\n");
                 return EXIT_SUCCESS;
             }
-            // Fehlerbehandlung
-            case '?':{
+                // Fehlerbehandlung
+            case '?': {
                 if (optopt) {
                     (void) fprintf(stderr, UNKNOWN_OPTION "'-%c'\n"HINT"\n", optopt);
                     return EXIT_FAILURE;
@@ -135,7 +135,7 @@ int main(int argc, char*argv[]) {
                 (void) fprintf(stderr, UNKNOWN_OPTION "'%s'\n"HINT"\n", argv[optind - 1]);
                 return EXIT_FAILURE;
             }
-            case ':':{
+            case ':': {
                 (void) fprintf(stderr, NO_REQUIRED_ARGUMENT"'%s'\n"HINT"\n", argv[optind - 1]);
                 return EXIT_FAILURE;
             }
@@ -146,16 +146,16 @@ int main(int argc, char*argv[]) {
     }
 
 
-    if(optind < argc) {
+    if (optind < argc) {
 
         inputfile = argv[optind]; // Mögliche Fehlerbehandlung folgt beim Einlesen der Datei
 
     } else {
-        (void) fprintf(stderr,NO_FILE_INPUT"\n"HINT"\n");
+        (void) fprintf(stderr, NO_FILE_INPUT"\n"HINT"\n");
         return EXIT_FAILURE;
     }
-    if(optind < argc - 1) {
-        (void) fprintf(stderr,TOO_MANY_OPTION"\n"HINT"\n");
+    if (optind < argc - 1) {
+        (void) fprintf(stderr, TOO_MANY_OPTION"\n"HINT"\n");
         return EXIT_FAILURE;
     }
 
