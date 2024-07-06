5b8fd5388cf033267e73a559c3d94b8996e51680
Jonah Zabel <develop.polony@gmx.net>
2024-07-06 16:51:29 +0200

    Updated to make tidy happy (;

--- src/main.c
+++ src/main.c
@@ -15,16 +15,16 @@ int main(int argc, char*argv[]) {
     unsigned int v2bBlockOffset = 4;
     unsigned int memoryLatency = 100;
     const char* tracefile = NULL;
-    const char* inputfile;
+    const char* inputfile = NULL;
     /*
      * die Werte von v2bBlockOffset und cycles wurden beliebig gewählt, da es in der Realität keinen v2bBlockOffset gibt
      * alle anderen Werte stammen von: Patterson, D. A., Hennessy, J. L. (). Computer Organization and Design: The Hardware/Software Interface. (4th ed.). Morgan Kaufman. Seite 503.
     */
-    int opt;
+    int opt = 0;
     static struct option long_options[] = {
-            {"cycles",required_argument,0,'c'},
-            {"blocksize",required_argument,0,'b'},
-            {"v2b-block-offset",required_argument,0,'o'},
+            {"cycles", required_argument, 0, 'c'},
+            {"blocksize", required_argument, 0, 'b'},
+            {"v2b-block-offset", required_argument, 0, 'o'},
             {"tlb-size",required_argument,0,'s'},
             {"tlb-latency", required_argument,0,'t'},
             {"memory-latency", required_argument,0,'m'},
@@ -37,8 +37,8 @@ int main(int argc, char*argv[]) {
     // Durchsucht die übergebenen Optionen ob Hilfe gedruckt werden soll
     for (int i = 1; i < argc; i++) {
         if (strncmp(argv[i], "-h",2) == 0 || strncmp(argv[i], "--help",6) == 0) {
-            fprintf(stderr,HELP_MSG"\n");
-            exit(EXIT_SUCCESS);
+            (void) fprintf(stderr,HELP_MSG"\n");
+            return EXIT_SUCCESS;
         }
     }
 
@@ -46,125 +46,117 @@ int main(int argc, char*argv[]) {
     while ((opt = getopt_long(argc, argv, ":c:b:o:s:t:m:f:h", long_options, NULL))!= -1){
         switch (opt){
             case 'c':{
-                char *endOfPointer;
+                char *endOfPointer = NULL;
                 errno = 0;
                 long tmp = strtol(optarg,&endOfPointer,10);
                 if(errno != 0|| *endOfPointer != '\0'|| tmp > INT32_MAX || tmp<0){
-                    fprintf(stderr,ILLEGAL_ARGUMENT_CYCLES"\n");
-                    exit(EXIT_FAILURE);
-                } else {
-                    cycles = (int) tmp;
+                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_CYCLES"\n");
+                    return EXIT_FAILURE;
                 }
+                cycles = (int) tmp;
                 break;
             }
             case 'b':{
-                char *endOfPointer;
+                char *endOfPointer = NULL;
                 errno = 0;
                 long tmp = strtol(optarg,&endOfPointer,10);
                 if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<=0){
-                    fprintf(stderr,ILLEGAL_ARGUMENT_BLOCKSIZE"\n");
-                    exit(EXIT_FAILURE);
-                } else {
-                    blocksize = (unsigned) tmp;
+                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_BLOCKSIZE"\n");
+                    return EXIT_FAILURE;
                 }
+                blocksize = (unsigned) tmp;
                 break;
             }
             case 'o':{
-                char *endOfPointer;
+                char *endOfPointer = NULL;
                 errno = 0;
                 long tmp = strtol(optarg,&endOfPointer,10);
                 if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    fprintf(stderr,ILLEGAL_ARGUMENT_V2B_BLOCK_OFFSET"\n");
-                    exit(EXIT_FAILURE);
-                } else {
-                    v2bBlockOffset = (unsigned) tmp;
+                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_V2B_BLOCK_OFFSET"\n");
+                    return EXIT_FAILURE;
                 }
+                v2bBlockOffset = (unsigned) tmp;
                 break;
             }
             case 's':{
-                char *endOfPointer;
+                char *endOfPointer = NULL;
                 errno = 0;
                 long tmp = strtol(optarg,&endOfPointer,10);
                 if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    fprintf(stderr,ILLEGAL_ARGUMENT_TLB_SIZE"\n");
-                    exit(EXIT_FAILURE);
-                } else {
-                    tlbSize = (unsigned) tmp;
+                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_TLB_SIZE"\n");
+                    return EXIT_FAILURE;
                 }
+                tlbSize = (unsigned) tmp;
                 break;
             }
             case 't':{
-                char *endOfPointer;
+                char *endOfPointer = NULL;
                 errno = 0;
                 long tmp = strtol(optarg,&endOfPointer,10);
                 if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    fprintf(stderr,ILLEGAL_ARGUMENT_TLB_LATENCY"\n");
-                    exit(EXIT_FAILURE);
-                } else {
-                    tlbLatency = (unsigned) tmp;
+                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_TLB_LATENCY"\n");
+                    return EXIT_FAILURE;
                 }
+                tlbLatency = (unsigned) tmp;
                 break;
             }
             case 'm':{
-                char *endOfPointer;
+                char *endOfPointer = NULL;
                 errno = 0;
                 long tmp = strtol(optarg,&endOfPointer,10);
                 if(errno != 0|| *endOfPointer != '\0'|| tmp > UINT32_MAX || tmp<0){
-                    fprintf(stderr,ILLEGAL_ARGUMENT_MEMORY_LATENCY"\n");
-                    exit(EXIT_FAILURE);
-                } else {
-                    memoryLatency = (unsigned) tmp;
+                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_MEMORY_LATENCY"\n");
+                    return EXIT_FAILURE;
                 }
+                memoryLatency = (unsigned) tmp;
                 break;
             }
             case 'f':{
                 tracefile = optarg;
-                FILE *file; // Erstelle eine Datei mit den Namen der künftigen Tracefile
+                FILE *file = NULL; // Erstelle eine Datei mit den Namen der künftigen Tracefile
                 file = fopen(tracefile,"w");
                 if(file == NULL) {
-                    fprintf(stderr,ILLEGAL_ARGUMENT_TRACEFILE"\n");
-                    exit(EXIT_FAILURE);
+                    (void) fprintf(stderr,ILLEGAL_ARGUMENT_TRACEFILE"\n");
+                    return EXIT_FAILURE;
                 }
                 fclose(file);
                 break;
             }
             case 'h':{
-                fprintf(stderr,HELP_MSG"\n");
-                exit(EXIT_SUCCESS);
+                (void) fprintf(stderr,HELP_MSG"\n");
+                return EXIT_SUCCESS;
             }
             // Fehlerbehandlung
             case '?':{
-                if(optopt) {
-                    fprintf(stderr, UNKNOWN_OPTION "'-%c'\n"HINT"\n", optopt);
-                    exit(EXIT_FAILURE);
-                }else {
-                    fprintf(stderr, UNKNOWN_OPTION "'%s'\n"HINT"\n", argv[optind - 1]);
-                    exit(EXIT_FAILURE);
+                if (optopt) {
+                    (void) fprintf(stderr, UNKNOWN_OPTION "'-%c'\n"HINT"\n", optopt);
+                    return EXIT_FAILURE;
                 }
+                (void) fprintf(stderr, UNKNOWN_OPTION "'%s'\n"HINT"\n", argv[optind - 1]);
+                return EXIT_FAILURE;
             }
             case ':':{
-
-                fprintf(stderr, NO_REQUIRED_ARGUMENT"'%s'\n"HINT"\n", argv[optind - 1]);
-                exit(EXIT_FAILURE);
+                (void) fprintf(stderr, NO_REQUIRED_ARGUMENT"'%s'\n"HINT"\n", argv[optind - 1]);
+                return EXIT_FAILURE;
             }
             default:
-                fprintf(stderr, UNKNOWN_OPTION "'%s'.\n", argv[optind - 1]);
-                exit(EXIT_FAILURE);
+                (void) fprintf(stderr, UNKNOWN_OPTION "'%s'.\n", argv[optind - 1]);
+                return EXIT_FAILURE;
         }
     }
 
 
     if(optind < argc) {
 
-        inputfile = argv[optind];// Mögliche Fehlerbehandlung folgt beim Einlesen der Datei
+        inputfile = argv[optind]; // Mögliche Fehlerbehandlung folgt beim Einlesen der Datei
 
     } else {
-        fprintf(stderr,NO_FILE_INPUT"\n"HINT"\n");
-        exit(EXIT_FAILURE);
+        (void) fprintf(stderr,NO_FILE_INPUT"\n"HINT"\n");
+        return EXIT_FAILURE;
     }
     if(optind < argc - 1) {
-        fprintf(stderr,TOO_MANY_OPTION"\n"HINT"\n");
-        exit(EXIT_FAILURE);
+        (void) fprintf(stderr,TOO_MANY_OPTION"\n"HINT"\n");
+        return EXIT_FAILURE;
     }
 
     // Read input file
@@ -173,7 +165,7 @@ int main(int argc, char*argv[]) {
     int requestCount = parseFile(inputfile, &requests);
 
     if (requestCount < 0) {
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     //TODO: Implement the simulation
@@ -195,7 +187,7 @@ int main(int argc, char*argv[]) {
         printf("%d: %d %u %u\n", i, requests[i].we, requests[i].addr, requests[i].data);
     }
 
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
 
 
